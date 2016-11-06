301dd3b651e2c3131906e9fc37c5152498c8d440
pizzamanblabla <everythingforpizza@gmail.com>
2016-11-07 02:34:27 +0300

    DEV-2: continue

---
+++ web/assets/js/main.js
@@ -0,0 +1,15 @@
+BreakingBet = {
+    init: function() {
+        this.initSportChangeHandler();
+    },
+    initSportChangeHandler: function() {
+
+    },
+    filter: function() {
+
+    }
+};
+
+$(document).ready(function () {
+    BreakingBet.init();
+});
